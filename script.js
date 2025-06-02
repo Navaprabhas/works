526629103a6fe8e57a1823f1af79f2d8bcebcffe
Navaprabhas <100205037+Navaprabhas@users.noreply.github.com>
2025-06-02 09:06:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const testimonials = [
       "When passion meets technology, innovation becomes your second nature.",
     name: "Nava Prabhas",
     designation: "AI & Tech Enthusisat",
-    src: ""C:\Users\NAVA PRABHAS\Downloads\unhash.jpg"",
+    src: ""unhash.jpg"",
   },
   {
     quote:
