12196e40999ac5d3b9332ee73c73f7c4ec785e55
Navaprabhas <100205037+Navaprabhas@users.noreply.github.com>
2025-06-02 08:59:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const testimonials = [
   },
   {
     quote:
-      "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
-    name: "Michael Rodriguez",
-    designation: "CTO at InnovateSphere",
-    src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "When passion meets technology, innovation becomes your second nature.",
+    name: "Nava Prabhas",
+    designation: "AI & Tech Enthusisat",
+    src: "https://unsplash.com/photos/man-is-wearing-a-vr-headset-G2nJfkEdUlk",
   },
   {
     quote:
